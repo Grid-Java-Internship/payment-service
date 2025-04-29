f6a6c20cff6cf1ed03cd350cad2ba3bd45209326
Milan Stošić <milanstosic00@gmail.com>
2025-04-29 11:05:51 +0200

    Deployment

--- src/main/java/com/internship/payment_service/PaymentServiceApplication.java
+++ src/main/java/com/internship/payment_service/PaymentServiceApplication.java
@@ -3,10 +3,12 @@ package com.internship.payment_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com.internship"},exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableFeignClients(basePackages = {"com.internship"})
+@EnableDiscoveryClient
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/internship/payment_service/proxy/UserProxy.java
+++ src/main/java/com/internship/payment_service/proxy/UserProxy.java
@@ -5,8 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "${microserviceUrls.user-service}",
-        configuration = UserServiceFeignConfiguration.class)
+@FeignClient(name = "user-service", configuration = UserServiceFeignConfiguration.class)
 public interface UserProxy {
 
 
