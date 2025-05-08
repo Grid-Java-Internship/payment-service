2c16248b466227b3ac5cf418da5fcaed3d85facd
Stefan <stefan.segrt.0502@gmail.com>
2025-05-08 15:48:17 +0200

    Added api key for communication between services

--- src/main/java/com/internship/payment_service/proxy/UserProxy.java
+++ src/main/java/com/internship/payment_service/proxy/UserProxy.java
@@ -1,10 +1,12 @@
 package com.internship.payment_service.proxy;
 
+import com.internship.authentication_library.feign.interceptor.UserServiceFeignConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "${microserviceUrls.user-service}")
+@FeignClient(name = "user-service", url = "${microserviceUrls.user-service}",
+        configuration = UserServiceFeignConfiguration.class)
 public interface UserProxy {
 
 
